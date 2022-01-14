d64052ac30bbb092f4cd3246f0833b5f4651f472
Albert Akrong <alphaxsalt@gmail.com>
2022-01-14 22:11:13 +0000

    fixed width

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import "reset-css";
 import "simplebar/dist/simplebar.min.css";
+import Head from "next/head";
 
 const theme = extendTheme({
   components: {
@@ -20,6 +21,9 @@ const theme = extendTheme({
 const MyApp = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=1280, user-scalable=yes" />
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
